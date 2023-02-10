9e688c23542183aa6b55d96fabfe5c4c0777cf9a
Isa Sierra <siemeris@gmail.com>
2023-02-10 17:03:39 +0100

    button

--- client/ecommerce/src/components/CardOfertas.jsx
+++ client/ecommerce/src/components/CardOfertas.jsx
@@ -4,19 +4,19 @@ import MesaTrabajo from '../img/6003841_Mesa_de_trabajo.png'
 const CardOfertas = () => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-    <img className="w-full" src={MesaTrabajo} alt="Mesa Trabajo"/>
-    <div className="px-6 py-4">
-      <div className="font-bold text-xl mb-2">Black Friday</div>
-      <p className="text-gray-700 text-base">
-        Parlante portátil Bluetooth Y669
-      </p>
+      <img className="w-full" src={MesaTrabajo} alt="Mesa Trabajo" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">Black Friday</div>
+        <p className="text-gray-700 text-base">
+          Parlante portátil Bluetooth Y669
+        </p>
+      </div>
+      <div className="px-6 pb-2">
+        <button className="w-52 bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-lg flex justify-center">
+          Comprar           
+        </button>
+      </div>
     </div>
-    <div className="px-6 pb-2">
-     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-  Agregar
-</button>
-    </div>
-  </div>
   )
 }
 
