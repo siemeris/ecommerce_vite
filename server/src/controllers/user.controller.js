6a59a1de74b408e37dded7fee880538e02467d5a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-02-10 16:00:42 -0600

    creando el archivo user.controller.js y creando el controller login

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,30 @@
+const AppError = require('../utils/appError');
+const tryCatch = require('../utils/tryCatch');
+
+const User = require('../models/userModel');
+
+exports.login = tryCatch(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+
+  const userFind = await User.findOne({ email, password });
+
+
+  if (!userFind) {
+    return next(new AppError('User not found', 404));
+  }
+
+  return res.status(200).json({
+    code: 200,
+    status: 'success',
+    data: {
+      user: userFind,
+    }
+  }).send();
+
+}); 
+
+
