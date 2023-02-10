20be492d89f3ac01e6fe99443532f190cf2ba1c3
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-02-10 14:52:56 -0600

    quitando async/await en la funcion connectDB, quitando userCreateIndex: true, useFindAndModify: false, ya que daba error de conexion asi la base de datos

--- server/src/utils/database.js
+++ server/src/utils/database.js
@@ -3,19 +3,16 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // connect database
-const connectDB = async () => {
-  try {
+const connectDB = () => {
 
-    const conn = await mongoose.connect(process.env.DB_MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    });
-    
-  } catch (error) {
-    console.log(error);
-  }
+  mongoose.connect(process.env.DB_MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false
+
+  }).then(() => {})
+    .catch(err => console.log('ERRRO EN LA BASE DE DATOS >>>>> ', err));
 }
 
 
