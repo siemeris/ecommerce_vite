38d94c6583b8e3b80ce05f628a4681e9e7bf0727
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-02-12 14:43:01 -0600

    creando el controllers resgiter user

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 const AppError = require('../utils/appError');
 const tryCatch = require('../utils/tryCatch');
 
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/usersModels');
 
 exports.login = tryCatch(async (req, res, next) => {
@@ -26,5 +28,46 @@ exports.login = tryCatch(async (req, res, next) => {
   }).send();
 
 }); 
+
+exports.Register = tryCatch(async (req, res, next) => { 
+  const {
+    email, 
+    password,
+    nameUser,
+    nameFull,
+  } = req.body;  
+
+  // encontrar en la base de datos si existe el email
+  const userFind = await User.findOne({ email });
+
+  if (userFind) {
+    return next(new AppError('este usuario ya esta registrado', 404));
+  }
+
+  // encriptar contraseña
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const userNew = {
+    email,
+    password: passwordHash,
+    nameUser,
+    nameFull,
+  }
+
+  // resgistrando el usuario en el modelo User
+  const user = await User.create(userNew);
+
+  user.password = undefined;
+
+  return res.status(200).json({
+    code: 200,
+    status: 'success',
+    data: {
+      user,
+    }
+  });
+
+});
 
 
