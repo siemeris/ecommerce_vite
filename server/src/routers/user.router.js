3fecc52e066d5186d6c0717a6e07fc0e8b13910b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-02-12 14:59:41 -0600

    agregando el endpoint /register

--- server/src/routers/user.router.js
+++ server/src/routers/user.router.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const {
   login,
+  Register
 } = require('../controllers/user.controller')
 
 const router = express.Router();
 
 router.post('/login', login);
+router.post('/register', Register);
 
 module.exports = { userRouter: router }; 
