292612b8df124d3c236b3223a5aa793049004b5b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-02-12 22:53:20 -0600

    quitando el password para buscar usuario

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -12,7 +12,7 @@ exports.loginUser = tryCatch(async (req, res, next) => {
     return next(new AppError('Please provide email and password', 400));
   }
 
-  const userFind = await User.findOne({ email, password });
+  const userFind = await User.findOne({ email });
 
 
   if (!userFind) {
