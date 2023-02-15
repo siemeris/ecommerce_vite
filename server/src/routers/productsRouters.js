b93a23e3bf47ea8d405f44c03d9cbe50234d2970
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-02-15 16:32:06 -0600

    borrando comentarios inecesarios

--- server/src/routers/productsRouters.js
+++ server/src/routers/productsRouters.js
@@ -3,9 +3,7 @@ const router = express.Router();
 
 const Product = require('../controllers/productsControllers')
 
-// PATH /API
-// 
-// router.get('/filter', products);
+
 router.post('/computadoras', Product.create);
 router.put('/computadoras:id', Product.update);
 router.delete('/computadoras:id', Product.delete);
