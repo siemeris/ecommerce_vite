4bf72db0315e57eb2c42c0c5ecfe26e02cff87af
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-02-15 16:13:26 -0600

    creando el middleware productoRegisterBody

---
+++ server/src/middlewares/productBody.middleware.js
@@ -0,0 +1,34 @@
+const { body } = require('express-validator'); 
+
+exports.producRegisterBody = [
+  body('title')
+    .notEmpty()
+    .withMessage('El titulo es requerido')
+    .isString()
+    .withMessage('El titulo debe ser un string'),
+  body('model')
+    .notEmpty()
+    .withMessage('El modelo es requerido')
+    .isString()
+    .withMessage('El modelo debe ser un string'),
+  body('brand')
+    .notEmpty()
+    .withMessage('La marca es requerida')
+    .isString()
+    .withMessage('La marca debe ser un string'),
+  body('price')
+    .notEmpty()
+    .withMessage('El precio es requerido')
+    .isNumeric()
+    .withMessage('El precio debe ser un numero'),
+  body('description')
+    .notEmpty() 
+    .withMessage('La descripcion es requerida')
+    .isString()
+    .withMessage('La descripcion debe ser un string'),
+  body('caracteristicas')
+    .notEmpty()
+    .withMessage('Las caracteristicas son requeridas')
+    .isArray()
+    .withMessage('Las caracteristicas deben ser un array'),
+]
