35f37408747eedea1023ce618b75e959664405da
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-02-15 16:38:31 -0600

    importando los middlewares producRegisterBody y valide, para posterior ponerlos en el router de crear productos

--- server/src/routers/productsRouters.js
+++ server/src/routers/productsRouters.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+// middelwares
+const {
+  producRegisterBody
+} = require('../middlewares/productBody.middleware')
+const {
+  validate
+} = require('../middlewares/body.middleware')
+
 const Product = require('../controllers/productsControllers')
 
-
-router.post('/computadoras', Product.create);
+router.post('/computadoras', validate(producRegisterBody), Product.create);
 router.put('/computadoras:id', Product.update);
 router.delete('/computadoras:id', Product.delete);
 
