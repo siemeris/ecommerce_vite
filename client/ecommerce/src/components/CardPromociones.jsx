c3442ee8e1863b6775697353f538e94f337e5aa0
Isa Sierra <siemeris@gmail.com>
2023-02-16 17:21:05 +0100

    promociones card adjusted

--- client/ecommerce/src/components/CardPromociones.jsx
+++ client/ecommerce/src/components/CardPromociones.jsx
@@ -10,18 +10,20 @@ const CardPromociones = () => {
             <p className='text-xl pt-16 -rotate-90 font-extrabold transform text-center inline-block'> AUDÍFONOS LOGITECH</p>
            
             </div>
-            <div className="px-6 py-4">
+            <div className="px-4 py-4">
                 <div className="font-bold text-3xl mb-2">$ 100</div>
                 <div className="flex justify-between">
                     <p className="text-gray-700 text-xl font-semibold">
                         Audífono Logitech G G335 Ligthspeed
                     </p>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                    <div className='my-auto ml-5'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                     </svg>
+                    </div>
                 </div>
             </div>
-            <div className="px-6 py-4">
+            <div className="px-4 py-4">
                 <div className="flex justify-between">
                     <div className="flex text-yellow-400">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="#FBBF24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
