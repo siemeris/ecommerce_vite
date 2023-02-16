ba90948509eaebbeb462a5c4c6be919c96b2f2cf
Isa Sierra <siemeris@gmail.com>
2023-02-16 16:47:28 +0100

    cleaning code

--- client/ecommerce/src/components/Home.jsx
+++ client/ecommerce/src/components/Home.jsx
@@ -7,25 +7,24 @@ import CardOfertas from './CardOfertas'
 const Home = () => {
   return (
     <div>
-    <p className="text-sm font-medium leading-4 ml-4 my-8">PROMOCIONES</p>
-    {/* <div className="flex overflow-x-scroll w-screen"> */}
+      <p className="text-sm font-medium leading-4 ml-4 my-8">PROMOCIONES</p>
       <div className="flex flex-no-wrap overflow-x-scroll scrolling-touch items-start mb-8">
-    <CardPromociones />
-    <CardPromociones />
-    <CardPromociones />
-    </div>
-    <p className="text-sm font-medium leading-4 ml-4 my-8">DESTACADOS</p>
-    <div className="flex flex-col md:flex-row">
-    <CardDestacados />
-    <CardDestacados />
-    <CardDestacados />
-    </div>
-    <p className="text-sm font-medium leading-4 ml-4 my-8">OFERTAS</p>
-    <div className="flex flex-no-wrap overflow-x-scroll scrolling-touch items-start mb-8">
-    <CardOfertas />
-    <CardOfertas />
-    <CardOfertas />
-    </div>
+        <CardPromociones />
+        <CardPromociones />
+        <CardPromociones />
+      </div>
+      <p className="text-sm font-medium leading-4 ml-4 my-8">DESTACADOS</p>
+      <div className="flex flex-col md:flex-row">
+        <CardDestacados />
+        <CardDestacados />
+        <CardDestacados />
+      </div>
+      <p className="text-sm font-medium leading-4 ml-4 my-8">OFERTAS</p>
+      <div className="flex flex-no-wrap overflow-x-scroll scrolling-touch items-start mb-8">
+        <CardOfertas />
+        <CardOfertas />
+        <CardOfertas />
+      </div>
     </div>
   )
 }
