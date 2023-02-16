a5433a1876d514e2957e9b24223c9f77e418d070
Isa Sierra <siemeris@gmail.com>
2023-02-16 19:11:22 +0100

    login component and route

--- client/ecommerce/src/components/MainRoutes.jsx
+++ client/ecommerce/src/components/MainRoutes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import CheckOut from './CheckOut'
 import Home from './Home'
 import ListaDeProductos from './ListaDeProductos'
+import Login from './Login'
 
 
 
@@ -12,6 +13,7 @@ const MainRoutes = () => {
         <Routes>
 
           <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
           <Route path='/checkout' element={<CheckOut />} />
           {/* Con esta ruta, se filtrarian celulares, tablets, compus, lo que sea */}
           <Route path='/productos/:categoriaProductos' element={<ListaDeProductos />}/>
