a51e97f8be2780cae6241aea531bb0b73b272e4c
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-02-19 00:41:45 -0600

    creando el controller registerSeller

--- server/src/controllers/seller.controllers.js
+++ server/src/controllers/seller.controllers.js
@@ -7,3 +7,28 @@ const bycript = require('bcryptjs');
 
 // models
 const Seller = require('../models/sellerModels');
+
+exports.registerSeller = tryCatch(async (req, res, next) => { 
+  const { name, email, password } = req.body;
+  const hashedPassword = await bycript.hash(password, 12);
+  const seller = await Seller.create({
+    name,
+    email,
+    password: hashedPassword
+  });
+
+  if (!seller) {
+    return next(new AppError('Seller not created', 400));
+  }
+
+  seller.save();
+  
+  seller.password = undefined;
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      seller
+    }
+  });
+});
