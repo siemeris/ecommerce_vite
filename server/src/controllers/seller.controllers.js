e204ee8ea864dbeb53a4e948062861a39ed25ae5
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-02-19 00:38:56 -0600

    creando el archivo seller.controllers.js y importando el AppError, tryCatch, bycript y model Seller

---
+++ server/src/controllers/seller.controllers.js
@@ -0,0 +1,9 @@
+// utils
+const AppError = require('../utils/AppError');
+const tryCatch = require('../utils/tryCatch');
+
+// bycript
+const bycript = require('bcryptjs');
+
+// models
+const Seller = require('../models/sellerModels');
