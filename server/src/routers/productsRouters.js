0b486591735d0235a27e097a4151fdc63ddbc3c4
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-02-18 22:18:20 -0600

    arreglando el router delete

--- server/src/routers/productsRouters.js
+++ server/src/routers/productsRouters.js
@@ -13,8 +13,8 @@ const Product = require('../controllers/productsControllers')
 
 
 router.post('/register', validate(producRegisterBody), Product.crearProducto);
-router.put('/update:id', Product.actualizarProducto);
-router.delete('/delete:id', Product.deleteProduct);
+router.put('/update/:id', Product.actualizarProducto);
+router.delete('/delete/:id', Product.deleteProduct);
 
 
 
