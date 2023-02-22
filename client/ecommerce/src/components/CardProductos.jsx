1a4dd9b40d6297c0f10f116c85f5b8685329d9a6
Isa Sierra <siemeris@gmail.com>
2023-02-22 23:27:44 +0100

    svg errors fixed

--- client/ecommerce/src/components/CardProductos.jsx
+++ client/ecommerce/src/components/CardProductos.jsx
@@ -3,7 +3,7 @@ import parlante from '../../public/img/parlante.png'
 
 const CardProductos = () => {
     return (
-        <div className="flex flex-row w-11/12 max-w-sm h-48 rounded-lg bg-white shadow-lg mx-auto mb-4">
+        <div className="flex flex-row w-11/12 max-w-sm rounded-lg bg-white shadow-lg mx-auto mb-4">
             <img className="w-24 h-auto object-cover rounded-l-lg" src={parlante} alt="" />
             <div className="flex flex-col justify-start w-full ml-4">
                 <h5 className="text-gray-900 text-2xl font-bold pt-6 mb-2 w-44 leading-7">Parlante Awei</h5>
@@ -13,11 +13,11 @@ const CardProductos = () => {
                         Bluetooth Flip 6 party boost potencia 20W, resistente al agua IP67
                     </p>
                 </div>
-                <div className="flex justify-between w-full">
+                <div className="flex justify-between w-full mb-2">
                     <p className="text-2xl font-bold">$ 100</p>
-                    <button type="button" className="inline-block px-1 py-1 mr-5 bg-white border-2 border-gray-800 text-gray-800 text-lg font-bold leading-tight uppercase rounded-full shadow-md hover:bg-gray-800 hover:text-white hover:shadow-lg transition duration-150 ease-in-out">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    <button type="button" className="inline-block px-1 py-1 mr-5 bg-white border-4 border-gray-800 text-gray-800 text-lg font-black leading-tight uppercase rounded-full shadow-md hover:bg-gray-800 hover:text-white hover:shadow-lg transition duration-150 ease-in-out">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="4.5" stroke="currentColor" className="w-5 h-5">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                         </svg>
 
                     </button>
