38a701a9ec4e535a544973569717f098e23f35e2
Isa Sierra <siemeris@gmail.com>
2023-02-23 11:36:43 +0100

    CardFiltroMarca y ListaDeProductos

--- client/ecommerce/src/components/ListaDeProductos.jsx
+++ client/ecommerce/src/components/ListaDeProductos.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom';
 import CardDestacados from './CardDestacados';
+import CardFiltroMarca from './CardFiltroMarca';
 import CardOfertas from './CardOfertas';
 import CardProductos from './CardProductos';
 import CardPromociones from './CardPromociones';
@@ -11,10 +12,15 @@ const ListaDeProductos = () => {
 
   const { categoriaProductos } = useParams()
   const [categoria, setCategoria] = useState()
+  const [marca, setMarca] = useState(false)
   console.log(categoriaProductos)
   useEffect(() => {
     setCategoria(categoriaProductos)
   }, [categoriaProductos])
+
+  const handleFilter = () => {
+    setMarca(!marca)
+  }
 
   return (
     <div className='w-full min-h-max pb-4'>
@@ -34,29 +40,35 @@ const ListaDeProductos = () => {
 
         <div className="flex flex-row justify-between">
           <p className="text-sm font-medium leading-4 ml-4 my-8 uppercase">{categoria}</p>
-          <div className="my-auto cursor-pointer">
+          <button className="my-auto cursor-pointer" onClick={handleFilter} >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
             </svg>
-          </div>
+          </button>
         </div>
-        <p className="text-sm font-medium leading-4 ml-4 my-8">PROMOCIONES</p>
-        <div className="flex flex-no-wrap overflow-x-scroll scrolling-touch items-start mb-8">
-          <CardOfertas />
-          <CardOfertas />
-          <CardOfertas />
-        </div>
-        <p className="text-sm font-medium leading-4 ml-4 my-8">PRODUCTOS</p>
-        <div className="flex flex-col">
-          <CardProductos />
-          <CardProductos />
-          <CardProductos />
-          <CardProductos />
-        </div>
+        <p className="text-sm font-medium leading-4 ml-4 mb-8">DESTACADOS</p>
 
+        {marca ?
+          <CardFiltroMarca />
+          :
+          <div>
+            <div className="flex flex-no-wrap overflow-x-scroll scrolling-touch items-start mb-8">
+              <CardOfertas />
+              <CardOfertas />
+              <CardOfertas />
+            </div>
+            <p className="text-sm font-medium leading-4 ml-4 my-8">PRODUCTOS</p>
+            <div className="flex flex-col">
+              <CardProductos />
+              <CardProductos />
+              <CardProductos />
+              <CardProductos />
+            </div>
+          </div>
+
+        }
       </div>
-    </div>
-  )
+    </div>)
 }
 
 export default ListaDeProductos
