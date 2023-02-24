aa0b75657b721bb9530517c3a9367453be724db8
Nico9934 <nico9934@gmail.com>
2023-02-23 23:10:58 -0300

    logica de agregar productos

--- client/ecommerce/src/components/Home.jsx
+++ client/ecommerce/src/components/Home.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CardPromociones from './CardPromociones'
 import CardDestacados from './CardDestacados'
 import CardOfertas from './CardOfertas'
-
+import axios from "axios"
 
 const Home = () => {
   
+  const [datos, setDatos] = useState([])
+
+  // Llamada a api 
+  const DATOS = "https://nocountry-production.up.railway.app/api/v1/products/allproducts"
+
+  useEffect( () => {
+    const getProducts = async () => {
+      try {
+        const {data} = await axios.get(DATOS)
+        console.log(data)
+      }
+      catch (error) {
+        console.log(error)
+      }
+    }
+
+    getProducts()
+  },[])
+  
+
   return (
     <div className='w-11/12 m-auto'>
       <p className="font-bold leading-4 ml-4 my-8">PROMOCIONES</p>
