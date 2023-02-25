686d7e322c945e2ad25cdac1ed6b0dc5b51601e0
Isa Sierra <siemeris@gmail.com>
2023-02-25 11:43:33 +0100

    fetching de datos en la Home

--- client/ecommerce/src/components/CardOfertas.jsx
+++ client/ecommerce/src/components/CardOfertas.jsx
@@ -4,10 +4,13 @@ import MesaTrabajo from '../img/6003841_Mesa_de_trabajo.png'
 
 
 
-const CardOfertas = () => {
+const CardOfertas = ({producto}) => {
+
+  const {title, price, subtitle, photos} = producto
+
   return (
     <div className="flex-none max-w-sm w-80 rounded overflow-hidden shadow-lg pb-4 mr-2">
-      <img className="object-cover" src={MesaTrabajo} alt="Mesa Trabajo" />
+      <img className="h-44 mx-auto" src={photos[0]} alt="Mesa Trabajo" />
 
       <div className='w-11/12 m-auto'>
 
@@ -15,7 +18,7 @@ const CardOfertas = () => {
       <div className="px-1 py-4">
         <div className="font-bold text-xl mb-2">Black Friday</div>
         <p className="text-gray-700 text-base">
-          Parlante portátil Bluetooth Y669
+          {subtitle}
         </p>
       </div>
 
--- client/ecommerce/src/components/Home.jsx
+++ client/ecommerce/src/components/Home.jsx
@@ -2,63 +2,91 @@ import React, { useEffect, useState} from 'react'
 import CardPromociones from './CardPromociones'
 import CardDestacados from './CardDestacados'
 import CardOfertas from './CardOfertas'
+import Spinner from './Spinner';
 
 
 const Home = () => {
-  const Products = () => {
-    const [datos, setDatos] = useState([]);
     
-    //api de mi data
-    
-    const DATOS = 'https://nocountry-production.up.railway.app/api/v1/products/allproducts';
-    
-    //Creamos una función para el useEffect
-    
-    useEffect(() => {
-        const getProducts = async () => {
-            try {
-                const { data } = await axios.get(DATOS);
-                setDatos(data.datos)
-                console.log(data.datos);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-    
-        getProducts()
-    }, []);
+  const [productosApi, setProductosApi] = useState([])
+  const [load, setLoad] = useState(true)
 
+  // Url del link de github con el .json
+  const url = "https://raw.githubusercontent.com/Nico9934/weatherApp/main/dataProductosNico.json"
+  const DATOS = "https://c9-52-mern.onrender.com/"
 
+  // Llamada a la api para guardar los productos 
+  useEffect( () => {
+    setLoad(true)
+
+    fetch(url)
+    .then( res => res.json())
+    .then( data => {
+      setProductosApi(data)
+      setLoad(false)
+    })
+
+  },[])
+  
   return (
     <div className='w-11/12 m-auto'>
       <p className="font-bold leading-4 ml-4 my-8">PROMOCIONES</p>
       <div className="flex flex-no-wrap overflow-x-scroll scrolling-touch items-start mb-8 gap-3">
-        <CardPromociones />
-        <CardPromociones />
-        <CardPromociones />
-        <CardPromociones />
-        <CardPromociones />
-        <CardPromociones />
-        <CardPromociones />
-        <CardPromociones />
-        <CardPromociones />
-        <CardPromociones />
+      {/* Mientras se carga se muestra el spinner */}
+      {load ?
+        <Spinner/>
+      :
+      // Mapea todos los productos para mostrar las promociones
+        productosApi.map( producto => {
+          if( producto.promocion) {
+            return <CardPromociones 
+              key={producto.id }
+              producto={producto}
+            />
+          }
+          
+        })
+      }
+     
       </div>
       <p className="text-sm font-medium leading-4 ml-4 my-8">DESTACADOS</p>
-      <div className="flex flex-col md:flex-row">
-        <CardDestacados />
-        <CardDestacados />
-        <CardDestacados />
+      <div className="flex flex-col md:grid md:grid-cols-2">
+        {/* Mientras se carga se muestra el spinner */}
+      {load ?
+        <Spinner/>
+      :
+      // Mapea todos los productos para mostrar las promociones
+        productosApi.map( producto => {
+          if( producto.destacados == 1) {
+            return <CardDestacados 
+              key={producto.id }
+              producto={producto}
+            />
+          }
+          
+        })
+      }
       </div>
       <p className="text-sm font-medium leading-4 ml-4 my-8">OFERTAS</p>
       <div className="flex flex-no-wrap gap-4 overflow-x-scroll scrolling-touch items-start mb-8">
-        <CardOfertas />
-        <CardOfertas />
-        <CardOfertas />
+              {/* Mientras se carga se muestra el spinner */}
+      {load ?
+        <Spinner/>
+      :
+      // Mapea todos los productos para mostrar las promociones
+        productosApi.map( producto => {
+          if( producto.blackfriday) {
+            return <CardOfertas 
+              key={producto.id }
+              producto={producto}
+            />
+          }
+          
+        })
+      }
       </div>
     </div>
   )
-}}
+}
 
 
 
