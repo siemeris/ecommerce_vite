7b192619b8f21e6d3ff1504a7a14f0438e43f2a4
Isa Sierra <siemeris@gmail.com>
2023-02-26 11:23:19 +0100

    filtroProductos Component

---
+++ client/ecommerce/src/components/FiltroProductos.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const FiltroProductos = ({menuVisible, setMenuVisible}) => {
+
+
+    const [categoriasVisible, setCategoriasVisible] = useState(false)
+
+  return (
+    <div className='absolute -inset-0 w-2/3 flex justify-evenly flex-col h-full bg-slate-50 p-4 z-30'>
+        
+        {/* Titulo y cierre */}
+        <div className="w-full flex justify-between content-center">
+            <h3 className="w-4/5 text-start uppercase font-bold m-auto">filtrar por:</h3>
+            <svg className="w-5 h-5 m-auto hover:cursor-pointer transition hover:text-red-600" onClick={() => setMenuVisible(!menuVisible)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        </div>
+
+
+
+        {/* Categorías y productos */}
+        <div className="w-full pb-5 flex flex-col border-b leading-loose border-gray-800  ">
+            <div className="w-full mb-2 flex justify-between">
+                <li className='list-none hover:cursor-pointer hover:text-indigo-700'>Marca</li>
+                <svg className='my-auto w-3 h-3 hover:cursor-pointer hover:text-indigo-700' onClick={() => setCategoriasVisible(!categoriasVisible)}  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" /></svg>
+            </div>
+        {/* Categorias */}
+        {
+            categoriasVisible 
+                &&
+                    <div className='flex flex-col text-center w-full transition-all '>
+                            <Link className="list-none hover:cursor-pointer hover:text-indigo-700 text-start ml-5" to={"/productos/computadoras"}>Logitech</Link>
+                            <Link className="list-none hover:cursor-pointer hover:text-indigo-700 text-start ml-5" to={"/productos/celulares"}>Asus</Link>
+                            <Link className="list-none hover:cursor-pointer hover:text-indigo-700 text-start ml-5" to={"/productos/auriculares"}>Iphone</Link>
+                            <Link className="list-none hover:cursor-pointer hover:text-indigo-700 text-start ml-5" to={"/productos/teclados"}>Microsoft</Link>
+                    </div>
+        }
+            <div className="w-full mb-2 flex justify-start">
+                <li className='list-none hover:cursor-pointer transition hover:text-indigo-700'>Mi Perfil</li>
+            </div>
+            <div className="w-full mb-2 flex justify-start">
+                <li className='list-none hover:cursor-pointer transition hover:text-indigo-700'>Ofertas</li>
+            </div>
+            <div className="w-full mb-2 flex justify-start">
+                <li className='list-none hover:cursor-pointer transition hover:text-indigo-700'>Contacto</li>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default FiltroProductos
--- client/ecommerce/src/components/ListaDeProductos.jsx
+++ client/ecommerce/src/components/ListaDeProductos.jsx
@@ -5,6 +5,7 @@ import CardFiltroMarca from './CardFiltroMarca';
 import CardOfertas from './CardOfertas';
 import CardProductos from './CardProductos';
 import CardPromociones from './CardPromociones';
+import FiltroProductos from './FiltroProductos';
 import Spinner from './Spinner';
 
 
@@ -19,6 +20,7 @@ const ListaDeProductos = () => {
     setCategoria(categoriaProductos)
   }, [categoriaProductos])
 
+  const [menuVisible, setMenuVisible] = useState(false)
   const handleFilter = () => {
     setMarca(!marca)
   }
@@ -53,20 +55,24 @@ const ListaDeProductos = () => {
           <div className='my-auto'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
               <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-            </svg> </div>
+            </svg> 
+            </div>
           <p className='text-sm text-gray-800 hover:cursor-pointer '>Productos</p>
         </div>
+
+
 
         <div className="flex flex-row justify-between">
           <p className="text-sm font-medium leading-4 ml-4 my-8 uppercase">{categoria}</p>
           <button className="my-auto cursor-pointer" onClick={handleFilter} >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+            <svg onClick={() => setMenuVisible(!menuVisible)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
             </svg>
           </button>
         </div>
         <p className="text-sm font-medium leading-4 ml-4 mb-8">DESTACADOS</p>
 
+        {menuVisible && <FiltroProductos menuVisible={menuVisible} setMenuVisible={setMenuVisible}/>}
         {marca ?
           <CardFiltroMarca />
           :
