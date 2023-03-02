096fc2da1c83eac871da12b904ed8b818deab626
Isa Sierra <siemeris@gmail.com>
2023-03-02 12:23:23 +0100

    lógica botones CardFiltroMarca

--- client/ecommerce/src/components/CardFiltroMarca.jsx
+++ client/ecommerce/src/components/CardFiltroMarca.jsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom';
-import CardDestacados from './CardDestacados';
-import CardOfertas from './CardOfertas';
-import CardProductos from './CardProductos';
-import CardPromociones from './CardPromociones';
-import parlante from '../../public/img/parlante.png'
+
 
 
 
@@ -12,6 +8,7 @@ const CardFiltroMarca = ({producto}) => {
 
   const { categoriaProductos } = useParams()
   const [categoria, setCategoria] = useState()
+  const { agregarProducto } = useCompra()
   const {title, price, subtitle, photos} = producto
   console.log(categoriaProductos)
   useEffect(() => {
@@ -24,7 +21,7 @@ const CardFiltroMarca = ({producto}) => {
           <div className='w-11/12 mx-auto'>
             <div className="flex justify-between">
               <p className="text-gray-800 text-xl font-bold w-4/5 mt-2">
-                {producto.title}
+                {title}
               </p>
               <div className="my-auto mr-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5"><path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" /></svg>
